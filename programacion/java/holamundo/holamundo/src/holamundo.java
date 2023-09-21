79a05f4d0c78fc304097ebb05db359b250dbeb67
atirram970 <atirram970@g.educaand.es>
2023-09-21 20:55:41 +0200

    actualización de repositorio local a remoto automatica

---
+++ programacion/java/holamundo/holamundo/src/holamundo.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+import java.awt.SystemColor;
+import java.util.*;
+public class holamundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//Comentario solo una línea
+		
+		/*
+			Comentario
+			Varias líneas
+		 */
+		
+		
+		System.out.println("Hola mundo!");
+	}
+
+}
