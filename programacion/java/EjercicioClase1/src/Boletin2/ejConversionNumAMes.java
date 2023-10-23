a17f878230f6b2e25c067b63d46bea57a8885f0c
atirram970 <atirram970@g.educaand.es>
2023-10-23 13:35:12 +0200

    subida archivos

--- programacion/java/EjercicioClase1/src/Boletin2/ejConversionNumAMes.java
+++ programacion/java/EjercicioClase1/src/Boletin2/ejConversionNumAMes.java
@@ -14,65 +14,71 @@ public class ejConversionNumAMes {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//Ejercicio calcular nota tipo test
-		
-		//Pedimos la cantidad de preguntas
-		Scanner scanner1=new Scanner(System.in);
-		System.out.println("Introduce el número de preguntas: ");
-		int numPreguntas=scanner1.nextInt();
-		
-		//Pedimos la cantidad de aciertos
-		Scanner scanner2=new Scanner(System.in);
-		System.out.println("Introduce el número de aciertos: ");
-		int numAciertos=scanner2.nextInt();
-		
-		//Pedimos la cantidad de errores
-		Scanner scanner3=new Scanner(System.in);
-		System.out.println("Introduce el número de errores: ");
-		int numErrores=scanner3.nextInt();
-		
-		//colocamos la formula
-		double Nota=((numAciertos-(numErrores/2f))*10f)/numPreguntas;
-		//transformamos la nota a int
-		int NotaRedondeada = (int) Nota;
-
-		//Prueba de errores
-		int totalRespuestas=(numPreguntas+numErrores);
-		if (totalRespuestas>numPreguntas)
+		//Ejercicio pedir num y devolver mes
+		Scanner scanner=new scanner(System.in);
+		System.out.println("Dame un numero del 1 al 12: ");
+		int nusMes=scanner.nextInt();
+		do 
 		{
-			System.out.println("Número érroneo de respuestas");
-			System.exit(0);
-		}
+			int numMes=scanner.nextInt();
+			
+			switch (numMes) 
+			{
+				case 1: 
+				{
+					System.out.println("Enero");
+				}
+				case 2: 
+				{
+					System.out.println("Febrero");
+				}
+				case 3: 
+				{
+					System.out.println("Marzo");
+				}
+				case 4: 
+				{
+					System.out.println("Abril");
+				}
+				case 5: 
+				{
+					System.out.println("Mayo");
+				}
+				case 6: 
+				{
+					System.out.println("Junio");
+				}
+				case 7: 
+				{
+					System.out.println("Julio");
+				}
+				case 8: 
+				{
+					System.out.println("Agosto");
+				}
+				case 9: 
+				{
+					System.out.println("Septiembre");
+				}
+				case 10: 
+				{
+					System.out.println("Octubre");
+				}
+				case 11: 
+				{
+					System.out.println("Noviembre");
+				}
+				case 12: 
+				{
+					System.out.println("Diciembre");
+				}
+				default:
+					System.out.println("Has introducido un valor incorrecto, intentalo de nuevo");
+			}
+		} 
+		while (numMes!=[1-12]);
 		
-		//Mostrar ambas notas en caso de error
-		System.out.println(Nota);
-		System.out.println(NotaRedondeada);
-		
-		//Encargado de elegir el mes segun el numero
-		switch ( NotaRedondeada ) 
-		{
-		case 0,1,2,3,4: {
-			System.out.println("Insuficiente");
-			break;
-		}
-		case 5,6: {
-			System.out.println("Suficiente");
-			break;
-		}
-		case 7,8: {
-			System.out.println("Notable");
-			break;
-		}
-		case 9,10: {
-			System.out.println("Sobresaliente");
-			break;
-		}
-		default:
-			System.out.println("Se ha introducido un valor incorrecto");
-		}
-		scanner1.close();
-		scanner2.close();
-		scanner3.close();
+		scanner scanner.close();
 	}
 
 }
