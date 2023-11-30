ec4845e1a55053b9168a4dfb84d1c7d7cc05c52e
atirram970 <atirram970@g.educaand.es>
2023-11-30 16:12:25 +0100

    actualizar

--- programacion/java/EjercicioClase1/src/Boletin8/ej5.java
+++ programacion/java/EjercicioClase1/src/Boletin8/ej5.java
@@ -12,87 +12,160 @@ import java.util.Scanner;
  */
 public class ej5 
 {
-	public static void main(String[] args)
+	// Ejercicio 5 Desarrolla un programa que, al proporcionarle una posición en un
+	// tablero de ajedrez, determine las casillas a las cuales un alfil ubicado en
+	// esa posición podría saltar. Conforme a la ilustración adjunta, se destaca que
+	// el alfil se desplaza exclusivamente en diagonal. El tablero consta de 64
+	// casillas, siendo las columnas designadas por las letras de la "a" a la "h" y
+	// las filas numeradas del 1 al 8.
+	// Ejemplo: Introduzca la posición del alfil: d5 El álfil puede moverse a las
+	// siguientes posiciones:h1 a2 g2 b3 f3 c4 e4 c6 e6 b7 f7 a8 g8
+	public static void main(String[] args) 
 	{
-		int [][] tabla = new int [8][8];
+		System.out.println("Introduce la posición del alfil, con el formato LETRANUMERO. Ejemplo: \"d5\"");
 		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Dame la posición en la que se encuentra el alfil (Ejemplo: \"d5\"): ");
 		String posicionAlfil = scan.nextLine();
+		scan.close();
 		char letra = posicionAlfil.charAt(0);
 		char numero = posicionAlfil.charAt(1);
-		
-		for (char fila = '8'; fila >= '1'; fila--)
+		//se pinta el tablero marcando el alfil con x
+		for (char fila = '8'; fila >= '1'; fila--) 
 		{
-			for (char columna = 'a'; columna <='h'; columna++)
+			for (char columna = 'a'; columna <= 'h'; columna++) 
 			{
 				if (letra == columna && numero == fila) 
 				{
 					System.out.print("X\t");
-				}
-				else
+				} 
+				else 
 				{
 					System.out.print(columna + "" + fila + "\t");
 				}
 			}
 			System.out.print("\n");
-			
 		}
+
+		System.out.print("\n");
+		System.out.print("\n");
+		System.out.print("\n");
+		System.out.print("\n");
+		
+		/*
+		 * OPCIÓN 1 -> guardar los movimientos en string
+		 * ""+FILA+COLUMNA
+		 * 
+		 * OPCIÓN 2 -> guardar los movimientos en char
+		 * Crearemos un array para guardar las filas que correspondera con la columna correspondiente en el mismo 
+		 * índice en el array de columnas
+		 */
+		char [] arrayFilas = new char[15];
+		char [] arrayColumnas = new char[15];
+		int indexArraysSolucion = 0;
+		
+		// Calculo la diagonal arriba-derecha
 		char fila = numero;
 		char columna = letra;
-		
-		//calculo la diagonal arriba-derecha
-		fila = numero;
-		columna = letra;
-		while (fila >= '1' && fila <= '8' && columna >= 'a' && columna <= 'h')
+		while (fila >= '1' && fila <= '8' && columna >= 'a' && columna <= 'h') 
 		{
-			if (fila != numero && columna != letra)
+			if (fila != numero && columna != letra) 
 			{
-				System.out.println("" + columna + fila);
+				System.out.print(" " + columna + fila);
+				arrayColumnas[indexArraysSolucion] = columna;
+				arrayFilas[indexArraysSolucion] = fila;
+				indexArraysSolucion++;
 			}
 			columna++;
 			fila++;
 		}
-		
-		//calculo la diagonal abajo-derecha
+		System.out.print("\n");
+		// Calculo la diagonal abajo-derecha
 		fila = numero;
 		columna = letra;
-		while (fila >= '1' && fila <= '8' && columna >= 'a' && columna <= 'h')
+		while (fila >= '1' && fila <= '8' && columna >= 'a' && columna <= 'h') 
 		{
-			if (fila != numero && columna != letra)
+			if (fila != numero && columna != letra) 
 			{
-				System.out.println("" + columna + fila);
+				System.out.print(" " + columna + fila);
+				arrayColumnas[indexArraysSolucion] = columna;
+				arrayFilas[indexArraysSolucion] = fila;
+				indexArraysSolucion++;
 			}
 			columna++;
 			fila--;
-			
 		}
-		
-		//calculo la diagonal arriba-izquierda
+		System.out.print("\n");
+		// Calculo la diagonal abajo-izquierda
 		fila = numero;
 		columna = letra;
-		while (fila >= '1' && fila <= '8' && columna >= 'h' && columna <= 'a')
+		while (fila >= '1' && fila <= '8' && columna >= 'a' && columna <= 'h') 
 		{
-			if (fila != numero && columna != letra)
+			if (fila != numero && columna != letra) 
 			{
-				System.out.println("" + columna + fila);
+				System.out.print(" " + columna + fila);
+				arrayColumnas[indexArraysSolucion] = columna;
+				arrayFilas[indexArraysSolucion] = fila;
+				indexArraysSolucion++;
+			}
+			columna--;
+			fila--;
+		}
+		System.out.print("\n");
+		// Calculo la diagonal arriba-izquierda
+		// TODO: EXTRA. Una vez calculados los movimientos, pintarlos en rojo en la
+		// matriz
+		fila = numero;
+		columna = letra;
+		while (fila >= '1' && fila <= '8' && columna >= 'a' && columna <= 'h') 
+		{
+			if (fila != numero && columna != letra) 
+			{
+				System.out.print(" " + columna + fila);
+				arrayColumnas[indexArraysSolucion] = columna;
+				arrayFilas[indexArraysSolucion] = fila;
+				indexArraysSolucion++;
 			}
 			columna--;
 			fila++;
 		}
-				
-		//calculo la diagonal abajo-izquierda
-		fila = numero;
-		columna = letra;
-		while (fila >= '1' && fila <= '8' && columna >= 'a' && columna <= 'h')
+		
+		System.out.println("\n");
+		
+		//se pinta el tablero marcando el alfil con una x
+		//muestra movimientos disponibles de color rojo
+		
+		final String RESET = "\u001B[0m";
+		final String AZUL = "\u001B[34m";
+		
+		for (char filaColor = '8'; filaColor >= '1'; filaColor--) 
 		{
-			if (fila != numero && columna != letra)
+			
+			for (char columnaColor = 'a'; columnaColor <= 'h'; columnaColor++) 
 			{
-				System.out.println("" + columna + fila);
+				String s = AZUL + columnaColor + "" + filaColor + "\t" +  RESET;
+				
+				if (letra == columnaColor && numero == filaColor) 
+				{
+					System.out.print("X\t");
+				} 
+				else 
+				{
+					//logica pintar datos color
+					int index = indexArraysSolucion;
+					boolean encontrado = false;
+					String pintada = "";
+					while (index >= 0 && !encontrado)
+					{
+						if(arrayColumnas[index] == columnaColor && arrayFilas[index] == filaColor)
+						{
+							encontrado = true;
+							pintada = AZUL + columnaColor + "" + filaColor + "\t" + RESET;
+						}
+						index--;
+					}
+					System.out.println(pintada);
+				}
 			}
-			columna++;
-			fila++;
+			System.out.print("\n");
 		}
-		scan.close();
 	}
 }
--- programacion/java/EjercicioClase1/src/Boletin8/ej6.java
+++ programacion/java/EjercicioClase1/src/Boletin8/ej6.java
@@ -14,14 +14,56 @@ public class ej6
 {
 	public static void main(String[] args)
 	{
+		//inicializando variables
 		Scanner scan = new Scanner(System.in);
-		int [][] tabla = new int[3][3];
-		boolean finalizada = false;
-		do 
-		{
-			
-		} 
-		while (!finalizada);
+		boolean resultado = false;
+		char [][] tabla = new char[3][3];
 		
+		//primera ejecucion
+		for (char fila = 'c'; fila >= 'a'; fila--) 
+		{
+			System.out.print("" + fila + "\t");
+			for (char columna = '1'; columna <= '3'; columna++) 
+			{
+				System.out.print("-\t");
+			}			
+			System.out.println("\n");
+		}
+		System.out.println("\t1\t2\t3");
+		
+		//ejecucion en bucle hasta ganar, empate o perder
+		while (!resultado)
+		{
+			System.out.println("Dame una posición en la cual vas a seleccionar (por ejemplo \"b2\"):");
+			String posicionJugador = scan.nextLine();
+			
+			char letra = posicionJugador.charAt(0);
+			char numero = posicionJugador.charAt(1);
+			
+			for (char fila = '3'; fila >= '1'; fila--) 
+			{
+				for (char columna = 'a'; columna <= 'c'; columna++) 
+				{
+					if (letra == columna && numero == fila) 
+					{
+						System.out.print("X\t");
+					} 
+					else 
+					{
+						System.out.print(columna + "" + fila + "\t");
+					}
+				}
+			}
+		}
+
+		if (resultado == true)
+		{
+			System.out.println("Has ganado");
+		}
+		else
+		{
+			System.out.println("Has perdido");
+		}
+		scan.close();
 	}
 }
