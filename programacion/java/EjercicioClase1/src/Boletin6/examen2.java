599d3388420e1146f91d307b470505e1421ab463
atirram970 <atirram970@g.educaand.es>
2023-12-14 18:42:01 +0100

    actualizar

---
+++ programacion/java/EjercicioClase1/src/Boletin6/examen2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Boletin6;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class examen2 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		
+	}
+}
